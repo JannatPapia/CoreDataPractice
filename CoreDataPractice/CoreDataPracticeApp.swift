e19f41c9b2fa36884440cf09cc37e1d85d8ed025
PapiaMCode <papiamcode@gmail.com>
2022-06-11 10:34:00 -0700

    Initial Commit

---
+++ CoreDataPractice/CoreDataPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataPracticeApp.swift
+//  CoreDataPractice
+//
+//  Created by Jannatun Nahar Papia on 11/6/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
